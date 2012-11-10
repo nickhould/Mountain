85a3d24d8c3ce6becf402375f8ee518fd7858485
Jean-Nicholas <nick.hould@gmail.com>
2012-11-10 14:02:10 -0600

    Implement basic chart

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -15,7 +15,7 @@ class DashboardsController < ApplicationController
   def show
     @dashboard = Dashboard.find(params[:id])
     ga = GoogleAnalytics.new
-    #@visits = ga.per_day(:visits)
+    #visits = ga.per_day(:visits)
     @visits = fake_data
     @sources = ga.profile.sources.sort_by{|e| e.visits.to_i}.reverse.take(10)
     @pages = ga.profile.pages.sort_by{|e| e.pageviews.to_i}.reverse.take(10)
@@ -94,36 +94,45 @@ class DashboardsController < ApplicationController
 
   #Delete me in production
   def fake_data
-    data = { "Fri, 09 Nov 2012"=>3, 
-            "Thu, 08 Nov 2012"=>2, 
-            "Wed, 07 Nov 2012"=>2, 
-            "Tue, 06 Nov 2012"=>2, 
-            "Mon, 05 Nov 2012"=>0, 
-            "Sun, 04 Nov 2012"=>0, 
-            "Sat, 03 Nov 2012"=>0, 
-            "Fri, 02 Nov 2012"=>0, 
-            "Thu, 01 Nov 2012"=>4, 
-            "Wed, 31 Oct 2012"=>1, 
-            "Tue, 30 Oct 2012"=>9, 
-            "Mon, 29 Oct 2012"=>2, 
-            "Sun, 28 Oct 2012"=>0, 
-            "Sat, 27 Oct 2012"=>1, 
-            "Fri, 26 Oct 2012"=>0, 
-            "Thu, 25 Oct 2012"=>3, 
-            "Wed, 24 Oct 2012"=>5, 
-            "Tue, 23 Oct 2012"=>14, 
-            "Mon, 22 Oct 2012"=>60, 
-            "Sun, 21 Oct 2012"=>2, 
-            "Sat, 20 Oct 2012"=>1, 
-            "Fri, 19 Oct 2012"=>4, 
-            "Thu, 18 Oct 2012"=>3, 
-            "Wed, 17 Oct 2012"=>2, 
-            "Tue, 16 Oct 2012"=>0, 
-            "Mon, 15 Oct 2012"=>4, 
-            "Sun, 14 Oct 2012"=>5, 
-            "Sat, 13 Oct 2012"=>4, 
-            "Fri, 12 Oct 2012"=>20, 
-            "Thu, 11 Oct 2012"=>68 } 
+    data = { "Fri, 09 Nov 2012".to_date =>3, 
+            "Thu, 08 Nov 2012".to_date =>2, 
+            "Wed, 07 Nov 2012".to_date =>2, 
+            "Tue, 06 Nov 2012".to_date =>2, 
+            "Mon, 05 Nov 2012".to_date =>0, 
+            "Sun, 04 Nov 2012".to_date =>0, 
+            "Sat, 03 Nov 2012".to_date =>0, 
+            "Fri, 02 Nov 2012".to_date =>0, 
+            "Thu, 01 Nov 2012".to_date =>4, 
+            "Wed, 31 Oct 2012".to_date =>1, 
+            "Tue, 30 Oct 2012".to_date =>9, 
+            "Mon, 29 Oct 2012".to_date =>2, 
+            "Sun, 28 Oct 2012".to_date =>0, 
+            "Sat, 27 Oct 2012".to_date =>1, 
+            "Fri, 26 Oct 2012".to_date =>0, 
+            "Thu, 25 Oct 2012".to_date =>3, 
+            "Wed, 24 Oct 2012".to_date =>5, 
+            "Tue, 23 Oct 2012".to_date =>14, 
+            "Mon, 22 Oct 2012".to_date =>60, 
+            "Sun, 21 Oct 2012".to_date =>2, 
+            "Sat, 20 Oct 2012".to_date =>1, 
+            "Fri, 19 Oct 2012".to_date =>4, 
+            "Thu, 18 Oct 2012".to_date =>3, 
+            "Wed, 17 Oct 2012".to_date =>2, 
+            "Tue, 16 Oct 2012".to_date =>0, 
+            "Mon, 15 Oct 2012".to_date =>4, 
+            "Sun, 14 Oct 2012".to_date =>5, 
+            "Sat, 13 Oct 2012".to_date =>4, 
+            "Fri, 12 Oct 2012".to_date =>20, 
+            "Thu, 11 Oct 2012".to_date =>68 } 
+  end
+
+
+  def genarate_string_for_chart(data_hash)
+    formated_hash = []
+    data_hash.each do |key, value|
+      formated_hash << [key.day, value] 
+    end
+    formated_hash.reverse
   end
 
 end

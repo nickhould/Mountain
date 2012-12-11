54e086625d4cef7b4c42bc37ca775ceaa059a877
Jean-Nicholas <nick.hould@gmail.com>
2012-12-10 22:08:47 -0600

    refactor default dashboard helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
 
 	def current_user?
+    # refactor this method to return true or false
 		@user ||= User.find_by_id(session[:user_id])
 	end
 
   def default_dashboard_url
-    if current_user && current.user.try(:dashboards)
+    if current_user && current_user.has_dashboard?
       dashboard_url(current_user.dashboards.first)
     else
       new_dashboard_url
